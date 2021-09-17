90556a22e9d9e4a6c2269534e5a7f661fb6c5fb2
Nicole Buendia <70274300+nbuendia@users.noreply.github.com>
2021-09-17 12:57:14 -0400

    astoroids clone, not fully functional

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
-
 import Phaser from "phaser";
+import PlayScene from "./scenes/playScene";
+import SpaceScene from "./scenes/spaceScene";
+
+const WIDTH = 1000;
+const HEIGHT = 600;
+const BIRD_POS = { x: WIDTH * 0.1, y: HEIGHT / 2 };
+
+const SHARED_CONFIG = {
+  width: WIDTH,
+  height: HEIGHT,
+  startPos: BIRD_POS,
+};
 
 const config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  ...SHARED_CONFIG,
   physics: {
-    default: 'arcade',
+    default: "arcade",
     arcade: {
-      gravity: { y: 200 }
-    }
+      debug: true,
+    },
   },
-  scene: {
-    preload: preload,
-    create: create
-  }
+  // scene: [new PlayScene(SHARED_CONFIG)],
+  scene: [new SpaceScene(SHARED_CONFIG)],
 };
 
 new Phaser.Game(config);
-
-function preload () {
-  this.load.image('sky', 'assets/sky.png');
-}
-
-function create () {
-  this.add.image(400, 300, 'sky');
-}
